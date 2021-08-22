e6caf99393172ba2f418f15ed4c2ebc68a88ed9f
imjord <dean.jay.goat@gmail.com>
2021-08-22 11:43:26 -0600

    added validators to comments and pizza creation

--- controllers/pizza-controller.js
+++ controllers/pizza-controller.js
@@ -42,15 +42,15 @@ const pizzaController = {
         .then(dbPizzaData => res.json(dbPizzaData))
         .catch(err => res.status(400).json(err));
     },
-    updatePizza({ params,body}, res) {
-        Pizza.findOneAndUpdate({_id: params.id }, body, { new: true })
+    updatePizza({ params, body }, res) {
+      Pizza.findOneAndUpdate({ _id: params.id }, body, { new: true, runValidators: true })
         .then(dbPizzaData => {
-            if(!dbPizzaData) {
-                res.status(404).json({ message: 'no pizza found with this id '});
-                return;
-            }
-            res.json(dbPizzaData);
-        }) 
+          if (!dbPizzaData) {
+            res.status(404).json({ message: 'No pizza found with this id!' });
+            return;
+          }
+          res.json(dbPizzaData);
+        })
         .catch(err => res.status(400).json(err));
     },
     deletePizza({ params }, res) {
