a59c584818f0f14bd4e41e5ed8749812f7b888b0
imjord <dean.jay.goat@gmail.com>
2022-04-11 19:54:01 -0600

    Added cloud mongo

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 
 
-mongoose.connect("mongodb://localhost/pizzadb");
+mongoose.connect("mongodb+srv://imjord:imjord123@cluster0.snso2.mongodb.net/pizza-db?retryWrites=true&w=majority");
 
 
 mongoose.set('debug', true);
